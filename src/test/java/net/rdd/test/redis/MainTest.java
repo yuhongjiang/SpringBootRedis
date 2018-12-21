69e9f7de5d0845edf76ee8ae55d63b295971c1ba
rdd <13707259624@163.com>
2018-12-21 10:49:26 +0800

    .gitignore文件

--- src/test/java/net/rdd/test/redis/MainTest.java
+++ src/test/java/net/rdd/test/redis/MainTest.java
@@ -4,6 +4,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by rdd on 2018/11/2.
  */
@@ -13,17 +16,23 @@ public class MainTest {
 
     public static void main(String[] args) {
 
-//        System.out.println(1111);
-//        System.out.println(2222);
-//        System.out.println(333);
-//        System.out.println(444);
-//        System.out.println(5555);
-//        System.out.println(6666);
+        String[] atp = {"Rafael Nadal", "Novak Djokovic",
+                "Stanislas Wawrinka",
+                "David Ferrer","Roger Federer",
+                "Andy Murray","Tomas Berdych",
+                "Juan Martin Del Potro"};
+        List<String> players =  Arrays.asList(atp);
 
-//        CacheMap.put("111", "fds");
-//
-//        Object o = CacheMap.get("111");
-//        System.out.println(o);
+// 以前的循环方式
+//        for (String player : players) {
+//            System.out.print(player + "; ");
+//        }
+
+// 使用 lambda 表达式以及函数操作(functional operation)
+//        players.forEach((player) -> System.out.print(player + "; "));
+//        players.forEach((player)->                System.out.println(player));
+// 在 Java 8 中使用双冒号操作符(double colon operator)
+        players.forEach(System.out::println);
 
     }
 
